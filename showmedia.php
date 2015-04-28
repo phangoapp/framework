4a0b7464a099718bebd7fda272fbd31268a93760
Antonio de la Rosa <webmaster@web-t-sys.com>
2015-04-28 16:20:15 +0200

    Fixes on media

--- showmedia.php
+++ showmedia.php
@@ -2,7 +2,19 @@
 
 include('autoload.php');
 
+use PhangoApp\PhaRouter\Routes;
 use PhangoApp\PhaView\View;
+
+include('config.php');
+
+//Define the views folders based on app installed.
+
+foreach(Routes::$apps as $app)
+{
+
+	View::$folder_env[]=$app.'/views';
+	
+}
 
 $view=new View();
 
