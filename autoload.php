e70409f6b570ca4b5cedf2bd6b6a0569b04e05fb
Antonio de la Rosa <webmaster@web-t-sys.com>
2015-04-28 02:00:03 +0200

    Fixes

--- autoload.php
+++ autoload.php
@@ -9,7 +9,7 @@ if(is_file(__DIR__ . '/vendor/composer/autoload_classmap.php'))
 	
 	if(count($classMap)>0)
 	{
-	
+		
 		spl_autoload_register(function ($className) use ($classMap) {
 
 		if (isset($classMap[$className])) {
